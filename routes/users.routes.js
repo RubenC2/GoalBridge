93c2fcfcff0b9a7ff73fe43e0f9a2bc841882072
RubenC2 <rubenct1993@gmail.com>
2024-11-13 17:11:42 +0100

    auhtOK

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,11 +1,12 @@
 const userController = require("../controllers/user.controller");
 const router = require('express').Router();
 const express = require('express');
-
+const authMiddleware = require('../middlewares/authMiddleware') // Queries SQL
 
 // Rutas funcionando
+router.get('/login', (req, res) => res.render('loginForm')); //
 router.get('/', userController.getUsers);
-router.post('/', userController.createUser);
+router.post('/', authMiddleware, userController.createUser);
 router.put('/put', userController.updateUser);
 router.get('/profile', (req, res) => res.render('userProfile')); // Vista de inicio de la app
 // router.delete('/user', apiController.createUser);
