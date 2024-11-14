4ba33eaceac7a9e905b33ce957e7089e48fa5e20
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-11-14 12:46:08 +0100

    logica favoritos sin terminar

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { addFavorite } = require('../controllers/api.controller');
 
-// Ruta para agregar una oferta a favoritos
+// Ruta para agregar una oferta a favoritoss
 router.post('/favorites', addFavorite);
 
 module.exports = router;
